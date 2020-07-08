63b46b49b98d28e52f44216b46047041fbf6d6e3
Rodrigo <cuitlagua@gmail.com>
2020-07-07 19:02:29 -0500

    Objeto Literal / mutabilidad

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -2,6 +2,17 @@ const persona = {
     nombre: "Tony",
     appelido: "Stark",
     edad: 45,
+    direccion: {
+        ciudad: "New York",
+        zip: 5562626,
+        lat: 14.3232,
+        lng: 34.65656,
+    }
 };
 
-console.table( persona )
+const persona2 = persona;
+persona2.nombre = "Peter";
+
+// Copiamos solo la referencia
+console.log( persona  )
+console.log( persona2 )
