8055860bb697e9c2fd4c7cfd010cd1692613f237
Rodrigo <cuitlagua@gmail.com>
2020-07-07 19:05:43 -0500

    Objetos Literales / Inmutable

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,18 +1,1 @@
-const persona = {
-    nombre: "Tony",
-    appelido: "Stark",
-    edad: 45,
-    direccion: {
-        ciudad: "New York",
-        zip: 5562626,
-        lat: 14.3232,
-        lng: 34.65656,
-    }
-};
-
-const persona2 = persona;
-persona2.nombre = "Peter";
-
-// Copiamos solo la referencia
-console.log( persona  )
-console.log( persona2 )
+console.log("oko");
