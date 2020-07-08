d2acce3f5e70a9f5b31cc042710b8016e7504e90
Rodrigo <cuitlagua@gmail.com>
2020-07-07 19:26:01 -0500

    Arreglo mutable, apunta a la misma ref

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,1 +1,11 @@
-console.log("oko");
+// Arreglos en JS
+const  arregloEmptySpaces = new Array( 100 ); // Arreglo sólo para crear espacios vacios
+
+const arreglo = [1,2,3,4];
+arreglo.push(5);
+
+let arreglo2 = arreglo;
+arreglo2.push(6)
+
+console.log( {arreglo} );
+console.log( {arreglo2} );
