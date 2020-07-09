eaf96d340c2793832e240ecb5479075ffd3d4686
Rodrigo <cuitlagua@gmail.com>
2020-07-08 22:24:37 -0500

    Arreglos

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,11 +1,1 @@
-// Arreglos en JS
-const  arregloEmptySpaces = new Array( 100 ); // Arreglo sólo para crear espacios vacios
-
-const arreglo = [1,2,3,4];
-arreglo.push(5);
-
-let arreglo2 = [...arreglo];
-arreglo2.push(6)
-
-console.log( {arreglo} );
-console.log( {arreglo2} );
+console.log("🚌")
