323ddee6c321016225bc38ea3596ab06eab31bb1
Rodrigo <cuitlagua@gmail.com>
2020-07-08 22:30:51 -0500

    Funcones / mala practica no expresarlas

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,1 +1,20 @@
-console.log("🚌")
+// Funciones en JS
+fuction saludar ( nombre ){
+    return `Hola, ${ nombre }`;
+};
+
+saludar = 30;
+
+// console.log( saludar('亀 Goku 🥕'));
+
+console.log( saludar );
+
+// FunctionDeclaration
+function foo(){
+    // Some code goes here...
+  }
+   
+  // FunctionExpression
+  var foo = function(){
+    // Some code goes here...
+  }
