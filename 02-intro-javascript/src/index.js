5f4573b154241bed75b1cf9ea12f2acaeb4f6acf
Rodrigo <cuitlagua@gmail.com>
2020-07-10 20:46:31 -0500

    array methods overview - part 1

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,1 +1,30 @@
-console.log("p");
+import { heroes } from './data/heroes';
+
+// find solo regresa un valor verdadero
+const getHeroById = (id) => heroes.find((heroe) => heroe['id'] === id);
+
+// filter es para regresar mas de un valor verdadero
+// use un arrow function pero este mataba mi valor de this que se usar para pasar el argumento a la función,
+// lo cambié por una función y le pude pasar el argumento!!!
+function filterByCompany(heroe) {
+	return heroe.owner === this.owner;
+}
+
+const getHeroByOwner = (owner) => heroes.filter(filterByCompany, { owner });
+
+// Tomar en cuenta que el accumulator es inmutable,
+// se pueden modificar los valores iniciales
+function getNameAndOwner(acc, current) {
+	return [ ...acc, { [current.name]: current.owner } ];
+}
+
+const newHero = {
+	name: 'Cuauhtlahuac',
+	owner: 'Panini'
+};
+
+const getHeroAndOwner = (hero) => heroes.reduce(getNameAndOwner, [ hero ]);
+
+console.log(getHeroById(2));
+console.log(getHeroByOwner('DC'));
+console.log(getHeroAndOwner(newHero));
