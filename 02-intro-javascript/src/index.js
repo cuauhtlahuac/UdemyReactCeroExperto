4a93f53cc8c2210e464fa8dee06859080c49dfa7
Rodrigo <cuitlagua@gmail.com>
2020-07-12 13:09:25 -0500

    async personal practice

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,1 +1,31 @@
-console.log("hello");
+// const https = require('https');
+const http = require('http');
+
+async function getCountryName(code) {
+
+	function waitForMe() {
+		return new Promise((res, rej) => {
+			setTimeout(() => {
+                console.log("Gracias por esperarme")
+				res('Thank you');
+			}, 2000);
+		});
+	}
+
+	function dontWaitForMe() {
+		return setTimeout(() => {
+			console.log('que malos, no me esperaron');
+		}, 2000);
+	}
+
+    await dontWaitForMe(); // este se ejecuta inmediatamente, si fuera asyncrono la próxima función
+                          // se ejecutaría hasta que esta terminara 2 segundos después
+	await waitForMe();
+
+}
+
+async function main() {
+	const name = await getCountryName('AF');
+	return name;
+}
+main();
