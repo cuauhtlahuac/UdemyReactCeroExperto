9363270159ba4613948589fb31df17e25ecb51b2
Rodrigo <cuitlagua@gmail.com>
2020-07-12 01:34:30 -0500

    Promises done

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,16 +1,1 @@
-import { getHeroById } from "js-bases/ModuleExport";
-
-const promesa = new Promise((res, rej) => {
-    console.log('La promesa inicia');
-    rej("Hubo un pedo"); // intencionalmente puse el reject antes para lanzar el waning
-    // así la promesa nunca se resuelve
-	setTimeout(() => {
-        const heroe = getHeroById(2);
-		res(heroe);
-	}, 2000);
-});
-
-promesa.then((response) => {
-	console.log(`Obtenemos un heroe que es: ${response.name}`);
-})
-.catch(error => console.warn(error));
+console.log("hello");
---
+++ 02-intro-javascript/src/js-bases/Promesas.js
@@ -0,0 +1,18 @@
+import { getHeroById } from 'js-bases/ModuleExport';
+
+const getHeroeByIdAsync = (id) => {
+	return new Promise((res, rej) => {
+		setTimeout(() => {
+			const heroe = getHeroById(id);
+			if (heroe) {
+				res(heroe);
+			} else {
+				rej('El heroe no se encontró');
+			}
+		}, 2000);
+	});
+};
+
+const myThen = heroe => console.log('Heroe: ', heroe);
+
+getHeroeByIdAsync(3).then(myThen).catch(console.warn);
