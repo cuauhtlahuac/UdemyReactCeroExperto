66442427ad3bb35b3100fa6835d84604b6e4686a
Rodrigo <cuitlagua@gmail.com>
2020-07-13 19:56:12 -0500

    Promisse done but can't get data yet

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,1 +1,9 @@
-console.log("hola")
+const apiKey = 'f5RDSoPgQf1hJ48d90Yk6rbYvrvP7e2l'; // Escribir después de la url y el "?"" sign
+
+const baseGifsURL = 'http://api.giphy.com/v1/gifs/random?api_key=';
+const baseStikersURL = 'http://api.giphy.com/v1/stickers/random?api_key=';
+
+const peticion = fetch(`${baseGifsURL}${apiKey}`);
+
+peticion.then((resp) => console.log(resp)).catch((err) => console.warn(err));
+ // Me regresa una promesa pero no puedo acceder a los datos
