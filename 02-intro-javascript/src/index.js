b40a303525888e3f778f1adbba8a450c6f11cdab
Rodrigo <cuitlagua@gmail.com>
2020-07-15 15:15:53 -0500

    Aync Await - Ejemplo de funcion sin async

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,3 +1,9 @@
-const getImagePromise = () => new Promise(resolve => resolve("http://osjsjisijis.com"));
-getImagePromise().then(console.log)
+// const getImagePromise = () => new Promise(resolve => resolve("http://osjsjisijis.com"));
+// getImagePromise().then(console.log)
+
+const getImagePromise = () => {
+    return 'http://ddijdijiod.com'
+}
+
+console.log(getImagePromise());
 
