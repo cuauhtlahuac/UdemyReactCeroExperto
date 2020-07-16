8f28f7d2feebd88c92413d3ce3218ccfef401139
Rodrigo <cuitlagua@gmail.com>
2020-07-16 18:25:11 -0500

    Primer componente React

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const saludo = (
+	<div>
+		<h1>Hola 🌎</h1>
+		<p>Todo 🆗</p>
+	</div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const divRoot = document.querySelector('#root');
+console.log(saludo);
+
+ReactDOM.render(saludo, divRoot);
