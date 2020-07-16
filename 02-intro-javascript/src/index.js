c10acd6b3b8379b637a23f4f3ceda87b708230d2
Rodrigo <cuitlagua@gmail.com>
2020-07-15 20:41:01 -0500

    Async Await - Finished, loop bonus

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,26 +1,5 @@
-// const getImagePromise = () => new Promise(resolve => resolve("http://osjsjisijis.com"));
-// getImagePromise().then(console.log)
-// Async Await
+import { getImagePromise } from "js-bases/11-AsyncAwait";
 
-// Async ya me regresa una promesa
-const getImagePromise = async () => {
-	const apiKey = 'f5RDSoPgQf1hJ48d90Yk6rbYvrvP7e2l';
-	const baseStikersURL = 'http://api.giphy.com/v1/stickers/random?api_key=';
-    console.log('before data');
-	// El await le dice, espera a que se ejecute esta línea de código antes de que se ejecute la siguiente
-	const resp = await fetch(`${baseStikersURL}${apiKey}`);
-    console.log('getting data:', resp);
-    // resp es una promesa, para obtener la data debemos parsearla con un json pero este metodo regresa una promesa
-    // debemos esperar a que esta promesa se parsee
-	const { data } = await resp.json();
-	console.log({ data });
-	const { url } = data.images.original;
-	// Creamos el elemento html
-	const img = document.createElement('img');
-	img.src = url;
-	document.body.append(img);
-
-	return img;
-};
-
-getImagePromise();
+for (let index = 0; index < 5; index++) {   
+    getImagePromise();
+}
---
+++ 02-intro-javascript/src/js-bases/11-AsyncAwait.js
@@ -0,0 +1,22 @@
+export const getImagePromise = async () => {
+	try {
+		const apiKey = 'f5RDSoPgQf1hJ48d90Yk6rbYvrvP7e2l';
+		const baseStikersURL = 'http://api.giphy.com/v1/stickers/random?api_key=';
+
+		const resp = await fetch(`${baseStikersURL}${apiKey}`);
+
+		const { data } = await resp.json();
+
+		const { url } = data.images.original;
+
+		const img = document.createElement('img');
+		img.src = url;
+        document.body.append(img);
+        
+		return img;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+getImagePromise();
