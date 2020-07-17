d1835fd9252e33fb966ee05cda3de129ab8e2c48
Rodrigo <cuitlagua@gmail.com>
2020-07-16 22:51:43 -0500

    Counter and Section finished

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,12 +1,16 @@
 import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 
-const CounterApp = ({ value }) => {
+const CounterApp = ({ value = 0 }) => {
 	const [ counter, setCounter ] = useState(value);
 
 	const handleAdd = (e) => {
 		// setCounter(counter + 1);
-		setCounter((c) => c+1); // el argumento es el counter state!!!
+		setCounter((c) => c + 1); // el argumento es el counter state!!!
+	};
+	const handleSub = (e) => {
+		// setCounter(counter + 1);
+		setCounter((c) => c - 1); // el argumento es el counter state!!!
 	};
 
 	return (
@@ -14,6 +18,8 @@ const CounterApp = ({ value }) => {
 			<h1>CounterApp</h1>
 			<h2> {counter} </h2>
 			<button onClick={handleAdd}>↑1</button>
+			<button onClick={() => setCounter(value)}>reset</button>
+			<button onClick={handleSub}>↓1</button>
 		</Fragment>
 	);
 };
