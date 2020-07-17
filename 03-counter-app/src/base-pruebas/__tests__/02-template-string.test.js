10bdb7f718e6eb19a6a88d1c45f9923c31f1706d
Rodrigo <cuitlagua@gmail.com>
2020-07-17 15:17:13 -0500

    testing function

--- 03-counter-app/src/base-pruebas/__tests__/02-template-string.test.js
+++ 03-counter-app/src/base-pruebas/__tests__/02-template-string.test.js
@@ -1,3 +1,17 @@
-test('should be true', () => {
-	expect(true).toBe(true);
+import { getSaludo } from 'base-pruebas/02-template-string';
+
+describe('02-templeate-string tests', () => {
+	test('getSaludo debe retornar Hola Cuauhtlahuac!', () => {
+		const name = 'Cuauhtlahuac';
+
+		const saludo = getSaludo(name);
+
+		expect(saludo).toBe('Hola Cuauhtlahuac!');
+	});
+
+	test('should render a default name Carlos', () => {
+		const saludo = getSaludo();
+
+		expect(saludo).toBe('Hola Carlos!');
+	});
 });
