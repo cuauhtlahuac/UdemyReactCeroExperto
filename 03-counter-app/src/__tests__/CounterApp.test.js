fb1d56314bfeff5123f49aa7de6e3331a4743b14
Rodrigo <cuitlagua@gmail.com>
2020-07-19 16:36:07 -0500

    Testing buttons events

--- 03-counter-app/src/__tests__/CounterApp.test.js
+++ 03-counter-app/src/__tests__/CounterApp.test.js
@@ -6,9 +6,13 @@ import toJSON from 'enzyme-to-json';
 import CounterApp from 'CounterApp';
 
 describe('<CounterApp /> tests', () => {
-	test('should render <CounterApp /> Correctly', () => {
-		const component = shallow(<CounterApp />);
 
+	let component; // Declaramos aqui para mantener el scope
+    
+    beforeEach(() => (component = shallow(<CounterApp />))); // Se va a ejecutar antes de cada prueba,
+                                                            // para reinicializar el componente
+
+	test('should render <CounterApp /> Correctly', () => {
 		expect(component).toMatchSnapshot();
 	});
 
@@ -20,5 +24,26 @@ describe('<CounterApp /> tests', () => {
 		expect(h2).toBe(` ${value} `);
 
 		expect(toJSON(component)).toMatchSnapshot();
+	});
+
+	test('should add 1 with "↑1" button', () => {
+		const btn1 = component.find('button').at(0); // at sirve para seleccionar la posición del elemento,
+		// en este caso también se puede usar first();
+		console.log(btn1.html()); // esto lo uso para solo para ver el elemento seleccionado es correcto
+		btn1.simulate('click'); // recibe dos argumentos, pero usamos solo uno, que es hacer click
+
+		const h2 = component.find('h2').text(); // Tomamos el componente updateado después de la simulación
+
+		expect(h2).toBe(` 11 `); // esperamos que sea el valor default 10 + 1 = 11
+	});
+
+	test('should subtract 1 with "↓1" button ', () => {
+		const btn2 = component.find('button').at(2);
+
+		btn2.simulate('click');
+
+		const h2 = component.find('h2').text();
+
+		expect(h2).toBe(` 9 `);
 	});
 });
