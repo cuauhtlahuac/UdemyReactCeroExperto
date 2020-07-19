0a2504d90887e239228b5d2959426a421dddf2ad
Rodrigo <cuitlagua@gmail.com>
2020-07-19 15:21:05 -0500

    Adding more tests

--- 03-counter-app/src/__tests__/PrimeraApp.test.js
+++ 03-counter-app/src/__tests__/PrimeraApp.test.js
@@ -7,11 +7,40 @@ import PrimeraApp from 'PrimeraApp';
 
 describe('<PrimeraApp />', () => {
 	test('should render <PrimeraApp /> correctly', () => {
-		
 		const saludo = 'Hola, Soy Goku!';
 
-		const wrapper = shallow(<PrimeraApp saludo={ saludo } />);
+		const wrapper = shallow(<PrimeraApp saludo={saludo} />);
 
-		expect( toJson( wrapper )).toMatchSnapshot();
+		expect(toJson(wrapper)).toMatchSnapshot();
+	});
+
+	test('should find a subtitle prop in ', () => {
+		const saludo = 'Hola, Soy Goku!';
+		const subtitle = 'Hola, Soy Subtitle';
+
+		const wrapper = shallow(<PrimeraApp saludo={saludo} subtitulo={subtitle} />);
+
+		const parrafo = wrapper.find('p').text(); // esto es lo mismo que hacer "document.querySelector();"
+		// From Enzime Docs: .find(selector) => ShallowWrapper
+		// > Find every node in the render tree that matches the provided selector.
+
+		expect(parrafo).toBe(subtitle);
+	});
+
+	test('should find one h1', () => {
+		const saludo = 'Hola, Soy Goku!';
+		const subtitle = 'Hola, Soy Subtitle';
+		const wrapper = shallow(<PrimeraApp saludo={saludo} subtitulo={subtitle} />);
+
+		const h1 = wrapper.find('h1');
+
+		expect(wrapper.find('h1')).toHaveLength(1);
+	});
+
+	test('should find props texts', () => {
+		const wrapper = shallow(<PrimeraApp saludo={'hfihf64684'} subtitulo={'foijoif1351'} />);
+
+		expect(wrapper.text()).toContain('hfihf64684');
+		expect(wrapper.text()).toContain('foi');
 	});
 });
