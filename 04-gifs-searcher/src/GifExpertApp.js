6765019cb005978d45c86875c6ced3313b516adf
Rodrigo <cuitlagua@gmail.com>
2020-07-19 19:54:00 -0500

    Created Gif Project and first configs done

---
+++ 04-gifs-searcher/src/GifExpertApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GifExpertApp = () => {
+    return (
+        <div>
+            <h2>Gif Expert App</h2>
+            <hr />
+        </div>
+    )
+}
+
+export default GifExpertApp
