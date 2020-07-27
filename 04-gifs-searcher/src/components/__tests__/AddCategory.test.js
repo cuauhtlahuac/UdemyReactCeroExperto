a136f6455c069dc14b0dfcaa6e479a1c3bb71745
Rodrigo <cuitlagua@gmail.com>
2020-07-27 18:12:58 -0500

    Testing functions with beforeEach and toBeCalled

--- 04-gifs-searcher/src/components/__tests__/AddCategory.test.js
+++ 04-gifs-searcher/src/components/__tests__/AddCategory.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import "@testing-library/jest-dom";
 import { shallow } from 'enzyme';
 import AddCategory from 'components/AddCategory';
 
 describe('AddCategory tests', () => {
     const setCategories =   jest.fn();
-    const component = shallow(<AddCategory setCategories={ setCategories }/>);
+    const setCategoryDictionary = jest.fn();
+    const setSameValue = jest.fn();
+    
+    let component = shallow( <p /> );
+
+    beforeEach(() => {
+
+        component = shallow( <AddCategory setCategories={ setCategories }/> );
+        component.find('form').simulate("submit", {preventDefault(){}});
+    })
     
 	test('should render correctly', () => {
 
@@ -21,5 +31,21 @@ describe('AddCategory tests', () => {
         
         expect( component.find('input').prop('value') ).toBe( value );
         expect( component ).toMatchSnapshot();
-    })
+    });
+
+    test('should not call setCategories with empty value', () => {
+
+        expect( setCategories ).not.toBeCalled();
+    });
+
+    test('should not call setCategoryDictionary with empty value', () => {
+        
+        expect( setCategoryDictionary ).not.toBeCalled();
+    });
+
+    test('should not call setSameValue with empty value', () => {
+        
+        expect( setSameValue ).not.toBeCalled();
+    });
+    
 });
