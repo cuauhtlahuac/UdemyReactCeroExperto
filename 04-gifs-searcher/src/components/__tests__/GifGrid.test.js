b71a69eeb234cd9cfad8ec96a03ae5e6b6960a72
Rodrigo <cuitlagua@gmail.com>
2020-07-28 15:36:34 -0500

    Tests: Adding values to useGetGifs mock

--- 04-gifs-searcher/src/components/__tests__/GifGrid.test.js
+++ 04-gifs-searcher/src/components/__tests__/GifGrid.test.js
@@ -20,7 +20,19 @@ describe('GifGrid tests', () => {
     });
 
     test('should render with mock change', () => {
+        const gifs = [
+            {
+                id: 'ass156',
+                title: "title",
+                link: "http://any.com.any",
+                gif: "http://any.com.gif",
+                webp_size: 5695,        
+            }];
 
+        useGetGifs.mockReturnValue({
+            data: gifs,
+            load: false,
+        }); 
 
         const component = shallow( <GifGrid category="luna"/> );
         expect( component ).toMatchSnapshot();
