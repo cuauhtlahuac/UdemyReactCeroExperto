c9bbe534082df79016f1c8873cd68580f8a1aff7
Rodrigo <cuitlagua@gmail.com>
2020-07-28 14:02:24 -0500

    Test using jest.mock and mocksReturnValue

--- 04-gifs-searcher/src/components/__tests__/GifGrid.test.js
+++ 04-gifs-searcher/src/components/__tests__/GifGrid.test.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import { shallow } from "enzyme"
+
 import GifGrid from 'components/GifGrid'
+import { useGetGifs } from 'hooks/useGetGifs'
+
+jest.mock('hooks/useGetGifs'); // con esto se finge cualquier llamada a la funcion interna
 
 describe('GifGrid tests', () => {
 
-    let component = <p />
-
-    beforeEach(()=>{
-        component = shallow( <GifGrid category=""/> );
-    });
-
     test('should render correctly', () => {
 
+        useGetGifs.mockReturnValue({
+            data:[],
+            load: true,
+        }); // simula el return de la funcion
+
+        const component = shallow( <GifGrid category="sol"/> );
+        expect( component ).toMatchSnapshot();
+    });
+
+    test('should render with mock change', () => {
+
+
+        const component = shallow( <GifGrid category="luna"/> );
         expect( component ).toMatchSnapshot();
 
     })
+    
+    
     
 })
