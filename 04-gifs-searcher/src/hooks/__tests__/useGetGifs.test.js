12d8ea3a201e45a4fb2babdbe0ffddca45c4a712
Rodrigo <cuitlagua@gmail.com>
2020-07-29 11:36:41 -0500

    Testing hooks with waitForNextUpdate

--- 04-gifs-searcher/src/hooks/__tests__/useGetGifs.test.js
+++ 04-gifs-searcher/src/hooks/__tests__/useGetGifs.test.js
@@ -1,15 +1,27 @@
 import '@testing-library/jest-dom'
 import { useGetGifs } from 'hooks/useGetGifs';
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 
 describe('useGetGifs Hook test', () => {
-	test('should return the initial state', () => {
-        const { result } = renderHook(() => useGetGifs('Muppets'));
-
+	it('should return the initial state', async() => {
+        const { result, waitForNextUpdate } = renderHook(() => useGetGifs());
+        
         const { data, loading } = result.current;
-
+        await waitForNextUpdate(); // EVITA EL ERROR POR EL UNMOUNT DEL COMPONENTE
+        
         expect( data.length ).toBe( 0 );
         expect( loading ).toBeTruthy();
+        
+    });
+    
+    it('should return an array of images, and loading false', async () => {
+        const { result, waitForNextUpdate } = renderHook(() => useGetGifs('Dragon Ball'));
+        await waitForNextUpdate();
+        
+        const { data, loading } = result.current;
 
-	});
+        expect( data.length ).toBe( 20 );
+        expect( loading ).toBeFalsy();
+    })
+    
 });
