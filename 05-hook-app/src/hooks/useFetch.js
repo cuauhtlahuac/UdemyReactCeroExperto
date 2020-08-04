04e214e83feaeb0c5be24a38bf8e5ffbb4d2d083
Rodrigo <cuitlagua@gmail.com>
2020-08-03 19:46:32 -0500

    Solving the unmount problem with useRef

--- 05-hook-app/src/hooks/useFetch.js
+++ 05-hook-app/src/hooks/useFetch.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export default function useFetch( url ) {
 
@@ -10,21 +10,40 @@ export default function useFetch( url ) {
 
 	const [ state, setState ] = useState( initialState );
 
+	const isMounted = useRef(true);
+
 	useEffect(() => {
-		setState( initialState );
+		
+		return () => {
+			// El único proposito de este use Effect es evitar que se haga
+			// un setState cuando el componente ya no está montado
+			// usando useRef
+
+			isMounted.current = false;
+		}
+	}, [isMounted])
+
+	useEffect(() => {
+		setState({
+			data: null,
+			loading: true,
+			error: null,
+		});
 
         fetch( url )
         .then( resp => resp.json() )
         .then( data => { 
 			setTimeout(() => {
-				
+				if(isMounted.current){
 				setState({
 					data,
 					loading: false,
 					error: null,
 				})
+			} else {console.log("no se monto");}
 			}, 4000);
-        })
+		})
+		console.log("llamando...");
 	}, [ url ]);
 
 	return state;
