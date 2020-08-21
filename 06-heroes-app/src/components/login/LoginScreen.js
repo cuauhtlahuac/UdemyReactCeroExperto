26ca6363c05e63dfa775d83fc60ba0bf836815b6
Rodrigo <cuitlagua@gmail.com>
2020-08-21 18:55:56 -0500

    Routes - print routes props

--- 06-heroes-app/src/components/login/LoginScreen.js
+++ 06-heroes-app/src/components/login/LoginScreen.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const LoginScreen = () => {
+const LoginScreen = (props) => {
+  const handleLogin = () => {
+    console.log(props);
+  }
+
   return (
-    <div>
+    <div className="m-5">
       <h1>Login</h1>
+      <hr />
+      <button className="btn btn-primary" onClick={handleLogin}> Login</button>
     </div>
   )
 }
