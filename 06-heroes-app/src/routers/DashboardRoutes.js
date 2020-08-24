ff0b0152c6ba9131d492ff83654204b62d57c537
Rodrigo <cuitlagua@gmail.com>
2020-08-23 19:27:02 -0500

    fix - Changed hero name

--- 06-heroes-app/src/routers/DashboardRoutes.js
+++ 06-heroes-app/src/routers/DashboardRoutes.js
@@ -3,7 +3,7 @@ import Navbar from 'components/ui/NavBar';
 import { Switch, Route } from 'react-router-dom';
 import DCScreen from 'components/dc/DCScreen';
 import MarvelScreen from 'components/marvel/MarvelScreen';
-import HeroesScreen from 'components/heroes/HeroesScreen';
+import HeroScreen from 'components/heroes/HeroScreen';
 
 const DashboardRoutes = () => {
 	return (
@@ -14,7 +14,8 @@ const DashboardRoutes = () => {
 				<Switch>
 					<Route path="/dc" component={ DCScreen } exact/>
 					<Route path="/marvel" component={ MarvelScreen } exact/>
-					<Route path="/" component={ HeroesScreen } exact/>
+					<Route path="/hero/:heroId" component={ HeroScreen } exact/>
+					<Route path="/" exact/>
 				</Switch>
 			</div>
 			 
