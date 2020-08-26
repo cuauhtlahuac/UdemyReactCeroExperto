577508d8dc7dbff75ed49fb9e6f0bd9ec18b7b4d
Rodrigo <cuitlagua@gmail.com>
2020-08-26 11:21:41 -0500

    starting new project

---
+++ 07-journal-app/src/JournalApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JournalApp = () => {
+  return (
+    <div>
+      <h1>Journal App</h1>
+    </div>
+  )
+};
+
+export default JournalApp;
---
+++ 07-journal-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import JournalApp from './JournalApp';
+
+ReactDOM.render(
+  <JournalApp />,
+  document.getElementById('root')
+);
