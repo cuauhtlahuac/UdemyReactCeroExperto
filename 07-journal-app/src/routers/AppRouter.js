4e98c44e212e5c27904869e1a599c2d8cb4aaadf
Rodrigo <cuitlagua@gmail.com>
2020-09-09 23:22:57 -0500

    Redux + Firebase - Persistencia de datos

--- 07-journal-app/src/routers/AppRouter.js
+++ 07-journal-app/src/routers/AppRouter.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
 
-import AuthRouter from './AuthRouter';
+import { firebase } from 'firebase/firebase-config';
 import JournalScreen from 'components/journal/JournalScreen';
 
+import AuthRouter from './AuthRouter';
+import { useDispatch } from 'react-redux';
+import { loginAction } from 'actions/auth';
+
 const AppRouter = props => {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		firebase.auth().onAuthStateChanged((user) => {
+			// es un observable
+			if (user && user.uid) {
+				dispatch(loginAction(user.uid, user.displayName));
+			}
+		});
+	}, []);
+
 	return (
-			<Router>
-				<Switch>
-					<Route path="/auth" component={AuthRouter} />
+		<Router>
+			<Switch>
+				<Route path="/auth" component={AuthRouter} />
 
-					<Route path="/" component={JournalScreen} exact />
+				<Route path="/" component={JournalScreen} exact />
 
-					<Route path="/" component={()=><h1>Not Found</h1>} />
-				</Switch>
-			</Router>
+				<Route path="/" component={() => <h1>Not Found</h1>} />
+			</Switch>
+		</Router>
 	);
 };
 
