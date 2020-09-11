8dd1d001a99831aa27061b4c9d9e3d92efe0da92
Rodrigo <cuitlagua@gmail.com>
2020-09-10 23:19:16 -0500

    isLogged - Set Auth and Loading page

--- 07-journal-app/src/routers/AppRouter.js
+++ 07-journal-app/src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
 
 import { firebase } from 'firebase/firebase-config';
@@ -11,14 +11,34 @@ import { loginAction } from 'actions/auth';
 const AppRouter = props => {
 	const dispatch = useDispatch();
 
-	useEffect(() => {
-		firebase.auth().onAuthStateChanged((user) => {
-			// es un observable
-			if (user && user.uid) {
-				dispatch(loginAction(user.uid, user.displayName));
-			}
-		});
-	}, []);
+	const [ checking, setChecking ] = useState(true);
+
+	const [ isLogged, setIsLogged ] = useState(false);
+
+	useEffect(
+		() => {
+			firebase.auth().onAuthStateChanged(user => {
+				// es un observable
+				if (user && user.uid) {
+					// solo usuarios autenticados
+					dispatch(loginAction(user.uid, user.displayName));
+					setIsLogged(true);
+				} else {
+					setIsLogged(false);
+				}
+
+				setChecking(false);
+			});
+		},
+		[ dispatch, setChecking, setIsLogged ],
+	);
+
+	if (checking)
+		return (
+			<div className="general-center">
+				<img src="https://www.downgraf.com/media/2019/05/Loader-animation-principle-freebie.gif" />
+			</div>
+		);
 
 	return (
 		<Router>
