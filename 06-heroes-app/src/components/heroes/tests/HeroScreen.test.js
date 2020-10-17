cefb4111f4857c3f255fd9adb00f151080691e2c
Rodrigo <cuitlagua@gmail.com>
2020-10-16 22:21:28 -0500

    test - Simulate Route to pass path

--- 06-heroes-app/src/components/heroes/tests/HeroScreen.test.js
+++ 06-heroes-app/src/components/heroes/tests/HeroScreen.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import HeroesScreen from '../HeroScreen';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
 
-describe('Tests <HeroScreen />', () => {
+describe('Tests <HeroScreen /> without hero', () => {
 	const historyMock = {
 		length: 10,
 		push: jest.fn(),
@@ -11,12 +11,34 @@ describe('Tests <HeroScreen />', () => {
 	};
 
 	const component = mount(
-		<MemoryRouter initialEntries={['/hero']}>
+		<MemoryRouter initialEntries={[ '/hero' ]}>
 			<HeroesScreen history={historyMock} />
 		</MemoryRouter>,
 	);
 
-	test('should show the component redirect if don\'t have arguments', () => {
+	test("should show the component redirect if don't have arguments", () => {
 		expect(component.find('Redirect').exists()).toBe(true);
+	});
+});
+
+describe('Tests <HeroScreen /> with hero', () => {
+	let component;
+
+	const historyMock = {
+		length: 10,
+		push: jest.fn(),
+		goBack: jest.fn(),
+	};
+
+	beforeEach(() => {
+		component = mount(
+			<MemoryRouter initialEntries={[ '/hero/marvel-spider' ]}>
+				<Route path="/hero/:heroId" component={HeroesScreen} />
+			</MemoryRouter>,
+		);
+	});
+
+	test('should show a hero if a param exists and is been found', () => {
+		expect(component.find('.col-6 h3').exists()).toBe(true);
 	});
 });
