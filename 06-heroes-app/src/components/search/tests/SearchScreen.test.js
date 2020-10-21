88e3878ff7b16831c62aaa0fb83f1858807ccb29
Rodrigo <cuitlagua@gmail.com>
2020-10-21 00:17:35 -0500

    test - SearchScreen query string tests

--- 06-heroes-app/src/components/search/tests/SearchScreen.test.js
+++ 06-heroes-app/src/components/search/tests/SearchScreen.test.js
@@ -13,10 +13,27 @@ describe('<SearchScreen /> Tests', () => {
 
 	test('should render correctly', () => {
 		expect(component).toMatchSnapshot();
-  });
-  
-  test('should show alert div', () => {
-    expect(component.find('div.alert-info').text()).toBe("You don't searching for heroes yet")
-  })
-  
+	});
+
+	test('should show alert div', () => {
+		expect(component.find('div.alert-info').text()).toBe(
+			"You don't searching for heroes yet",
+		);
+	});
+
+	describe('', () => {
+		const component = mount(
+			<MemoryRouter initialEntries={[ '/search?q=batman' ]}>
+				<Route path="/search" component={SearchScreen} />
+			</MemoryRouter>,
+		);
+		test('should update data when url change', () => {
+			expect(component.find('div.alert-info').exists()).not.toBe(true);
+
+			expect(component.find('h5.card-title').text()).toBe('Batman');
+		});
+		test('should clear the text input', () => {
+			expect(component.find('input[name="searchText"]').prop('value')).toBe('');
+		});
+	});
 });
