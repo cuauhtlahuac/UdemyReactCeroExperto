3b044784f5818c136141e162e3f31e682a49d52d
Rodrigo <cuitlagua@gmail.com>
2020-10-25 14:43:23 -0600

    calendar/config - screens created

---
+++ 08-calendar-app/src/components/auth/LoginScreen.js
@@ -0,0 +1,10 @@
+
+const LoginScreen = () => {
+  return (
+    <div>
+      <h1>LoginScreen</h1>      
+    </div>
+  )
+}
+
+export default LoginScreen
---
+++ 08-calendar-app/src/components/calendar/CalendarScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CalendarScreen = () => {
+  return (
+    <div>
+      <h1>CalendarScreen</h1>
+    </div>
+  )
+}
+
+export default CalendarScreen
