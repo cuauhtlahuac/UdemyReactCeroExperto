76c7d8b71335a9c95b12f3fba3311281ba896826
Rodrigo <cuitlagua@gmail.com>
2020-10-24 23:35:17 -0500

    Updates

---
+++ precache-manifest.e53884f0bdec98407084e45fd9ccd7f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b829d74b835db618a979d8779295f5",
+    "url": "/journal-app/index.html"
+  },
+  {
+    "revision": "9f430848f665287e98cf",
+    "url": "/journal-app/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "972f81637d0586d71b1c",
+    "url": "/journal-app/static/css/main.14b43f83.chunk.css"
+  },
+  {
+    "revision": "9f430848f665287e98cf",
+    "url": "/journal-app/static/js/2.12d2312d.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/journal-app/static/js/2.12d2312d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972f81637d0586d71b1c",
+    "url": "/journal-app/static/js/main.b6b1e968.chunk.js"
+  },
+  {
+    "revision": "7369199aa78be9551bdd",
+    "url": "/journal-app/static/js/runtime-main.01427f0e.js"
+  }
+]);
