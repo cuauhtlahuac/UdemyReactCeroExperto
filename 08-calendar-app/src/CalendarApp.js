d324432397f39ca61acc1b67d728891f7a78e1e9
Rodrigo <cuitlagua@gmail.com>
2020-10-25 18:17:29 -0600

    calendar/config - React Router

--- 08-calendar-app/src/CalendarApp.js
+++ 08-calendar-app/src/CalendarApp.js
@@ -1,10 +1,12 @@
+import AppRouter from "components/routers/AppRouter";
 
 function CalendarApp() {
-  return (
-    <div>
-      <h1>Calendar App</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Calendar App</h1>
+			<AppRouter />
+		</div>
+	);
 }
 
 export default CalendarApp;
---
+++ 08-calendar-app/src/components/routers/AppRouter.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import LoginScreen from 'components/auth/LoginScreen';
+import CalendarScreen from 'components/calendar/CalendarScreen';
+
+const AppRouter = () => {
+	return (
+		<Router>
+			<div>
+				<Switch>
+					<Route path="/" component={CalendarScreen} exact />
+
+					<Route path="/login" component={LoginScreen} exact />
+
+					<Redirect to="/" />
+				</Switch>
+			</div>
+		</Router>
+	);
+};
+
+export default AppRouter;
