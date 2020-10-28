ae47a4b0394b4d68e6cde2a0e98c1d1064e92f1d
Rodrigo <cuitlagua@gmail.com>
2020-10-27 23:01:54 -0600

    calendar - Basic Calendar - no styles

--- 08-calendar-app/src/components/calendar/CalendarScreen.js
+++ 08-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,14 +1,31 @@
-import NavBar from 'components/ui/NavBar'
-import React from 'react'
+import NavBar from 'components/ui/NavBar';
+import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: "koksoos",
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: "#fa09f5"
+  }
+]
 
 const CalendarScreen = () => {
-  return (
-    <div>
-      <NavBar />
-      <h1>CalendarScreen</h1>
+	return (
+		<div>
+			<NavBar />
+      <Calendar
+      localizer={localizer}
+      events={ events }
+      startAccessor="start"
+      endAccessor="end"
+    />
+		</div>
+	);
+};
 
-    </div>
-  )
-}
-
-export default CalendarScreen
+export default CalendarScreen;
