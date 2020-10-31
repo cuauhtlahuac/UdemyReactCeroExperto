9e3910c1c32307cb6ec1b2d19a1876a1bd786056
Rodrigo <cuitlagua@gmail.com>
2020-10-30 23:41:58 -0600

    calendar - full calendar first touchs

--- 08-calendar-app/src/components/calendar/CalendarScreen.js
+++ 08-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,10 +1,42 @@
 import React from 'react';
+
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import moment from 'moment';
+
+import NavBar from 'components/ui/NavBar';
+
+import CalendarEvent from './CalendarEvent';
+
+import 'moment/locale/es';
+
+moment.locale('es');
+
+const events = [
+	{
+		title: 'cumpleaños',
+		start: moment().toDate(),
+		end: moment().add(2, 'hours').toDate(),
+		bgcolor: '#fa09f5',
+	},
+];
 
 const CalendarScreenFull = () => {
 	return (
-		<FullCalendar plugins={[ dayGridPlugin ]} initialView="dayGridMonth" />
+		<div className="calendar-screen">
+			<NavBar />
+			<FullCalendar
+				plugins={[ dayGridPlugin ]}
+				initialView="dayGridMonth"
+				events={events}
+				eventContent={CalendarEvent}
+				headerToolbar={{
+					left: 'prev,next',
+					center: 'title',
+					right: 'dayGridDay,dayGridWeek, dayGridMonth',
+				}}
+			/>
+		</div>
 	);
 };
 
