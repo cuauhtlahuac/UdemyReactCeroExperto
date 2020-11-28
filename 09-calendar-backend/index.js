8ec5766b90de03a54ed2007b2ba9839c223b3373
Rodrigo <cuitlagua@gmail.com>
2020-11-28 11:31:38 -0600

    backend/create public files and showing

--- 09-calendar-backend/index.js
+++ 09-calendar-backend/index.js
@@ -3,12 +3,18 @@ const express = require('express');
 // crear servidor de express
 const app = express();
 
+// Directorio Público
+app.use( // use es un middleware más información http://expressjs.com/en/guide/writing-middleware.html#writing-middleware-for-use-in-express-apps
+   express.static(
+     'public' // The root argument specifies the root directory from which to serve static assets.
+  ) 
+)
 
 // Route definition takes the following structure: app.METHOD(PATH, HANDLER)
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
    res.json({"say": 'hi'}) // sending a json 
   });
-
+ */
 
 // Escuchar peticiones
 app.listen( 4000, () => 
