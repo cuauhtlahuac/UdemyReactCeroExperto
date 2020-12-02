6b297de8adb65e382e32e9e34f6f1418f7df9366
Rodrigo <cuitlagua@gmail.com>
2020-12-01 23:15:40 -0600

    backend/structuring login and register req

--- 09-calendar-backend/controllers/auth.js
+++ 09-calendar-backend/controllers/auth.js
@@ -2,15 +2,25 @@
 /* Anteriormente las teniamos en la misma ruta  pero se separo por praticidad*/
 
 const login = (req, res) => {
+	const { name, email, password } = req.body;
+
 	res.json({
 		ok: true,
 		msg: 'login',
-		user: req.body,
+		name,
+		email,
+		password,
 	});
 };
 
 const register = (req, res) => {
-	res.json({ ok: true, msg: 'register' });
+	const { email, password } = req.body;
+	res.json({
+		ok: true,
+		msg: 'register',
+		email,
+		password,
+	});
 };
 
 const renew = function(req, res) {
