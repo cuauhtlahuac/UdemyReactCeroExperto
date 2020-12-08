2f1ffdd94e4f672abe8d5dd7a3342760850b1872
Rodrigo <cuitlagua@gmail.com>
2020-12-07 22:56:14 -0600

    backend/validating if the user email already exist

--- 09-calendar-backend/controllers/auth.js
+++ 09-calendar-backend/controllers/auth.js
@@ -1,22 +1,34 @@
 const { validationResult } = require('express-validator');
-const User = require('../models/User')
+const User = require('../models/User');
 
-const login = async(req, res) => {
-	try{
-		const user = new User( req.body )
-		
-		await user.save()
-	
+const login = async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		// revisa si existe el email
+		let user = await User.findOne({ email });
+
+		if (user) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'The email already exist',
+			});
+		}
+
+		user = new User(req.body);
+
+		await user.save();
+
 		res.status(201).json({
 			ok: true,
-			msg: 'login',
+			uid: user.id,
+			name: user.name,
 		});
-
-	} catch(error) {
+	} catch (error) {
 		console.log(error);
 		res.status(500).json({
 			ok: false,
-			msg: 'Something went wrong. Please contact with to your administrator'
+			msg: 'Something went wrong. Please contact with to your administrator',
 		});
 	}
 };
