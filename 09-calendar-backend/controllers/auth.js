384c98d6b12bc21285403840d23962aa1e645042
Rodrigo <cuitlagua@gmail.com>
2020-12-09 23:22:01 -0600

    backend/login validations

--- 09-calendar-backend/controllers/auth.js
+++ 09-calendar-backend/controllers/auth.js
@@ -41,15 +41,43 @@ const register = async (req, res) => {
 	}
 };
 
-const login = (req, res) => {
+const login = async(req, res) => {
 	const { email, password } = req.body;
 
-	res.status(200).json({
-		ok: true,
-		msg: 'login',
-		email,
-		password,
-	});
+	try {
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			console.log("Email doesn't exists");
+			return res.status(400).json({
+				ok: false,
+				msg: 'Your email or password is incorrect',
+			});
+		}
+		
+		const validPassword = bcrypt.compareSync( password, user.password);
+		
+		if(!validPassword){
+			console.log("Password is incorrect");
+			return res.status(400).json({
+				ok: false,
+				msg: 'Your email or password is incorrect',
+			});
+		}
+
+		return res.status(200).json({
+			ok: true,
+			name: user.name,
+			uid: user.id,
+		});
+
+	} catch(error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'Something went wrong. Please contact with to your administrator',
+		});
+	}
 };
 
 const renew = function(req, res) {
