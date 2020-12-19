bcbd8baa02e7a1d0c5327c8d859c012742f706fd
Rodrigo <cuitlagua@gmail.com>
2020-12-18 20:14:19 -0600

    backend/saving event in DB

--- 09-calendar-backend/controllers/events.js
+++ 09-calendar-backend/controllers/events.js
@@ -1,28 +1,48 @@
+const Event = require('../models/Events');
+
 const getEvent = (req, res) => {
-	console.log(req.body);
+
 	return res.json({
 		ok: true,
 		msg: 'getEvent',
-		body: req.body
+		body: req.body,
 	});
 };
 
-const newEvent = (req, res) => {
-	return res.json({
-		ok: true,
-		msg: 'newEvent',
-	});
+const newEvent = async(req, res) => {
+	const event = new Event(req.body); // Creando una nueva instancia del Evento para la base de datos
+
+	try {
+
+		event.user = req.uid; // validamos el user id con referencia al modelo
+
+		const	savedEvent = await event.save(); // guardamos la instancia del evento, esperando primero a que no existan problemas
+
+		res.status(200).json({
+			ok: false,
+			event: savedEvent,
+		})
+
+	} catch (error) {
+
+		console.log(error);
+
+		return res.status(500).json({
+			ok: 'false',
+			msg: 'DB Error, please contact the admin',
+		});
+	}
 };
 
 const updateEvent = (req, res) => {
-  try{
-    return res.json({
-      ok: true,
-      msg: 'updateEvent',
-    });
-  }catch(error){
-    console.log(error);
-  }
+	try {
+		return res.json({
+			ok: true,
+			msg: 'updateEvent',
+		});
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 const deleteEvent = (req, res) => {
--- 09-calendar-backend/models/Events.js
+++ 09-calendar-backend/models/Events.js
@@ -3,23 +3,24 @@ const { Schema, model } = require('mongoose');
 const EventSchema = Schema({
 	title: {
 		type: String,
-		require: true,
+		required: true,
 	},
 	notes: {
 		type: String,
 	},
 	start: {
 		type: Date,
-		require: true,
+		required: true,
 	},
 	end: {
 		type: Date,
-		require: true,
-  },
-  user: {
-    type: Schema.Types.ObjectId, // Especificamos el id
-    ref: 'User', // Hacemos la referencia al esquema
-  }
+		required: true,
+	},
+	user: {
+		type: Schema.Types.ObjectId, // Especificamos el id
+		ref: 'User', // Hacemos la referencia al esquema
+		required: true,
+	},
 });
 
 module.exports = model('Event', EventSchema);
