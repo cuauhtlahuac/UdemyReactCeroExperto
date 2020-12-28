82f127a48554b46d2579b3a5a560bec5b6794499
Rodrigo <cuitlagua@gmail.com>
2020-12-28 10:17:08 -0600

    connect/Saga getting mock data

--- 08-calendar-app/src/sagas/authSaga.js
+++ 08-calendar-app/src/sagas/authSaga.js
@@ -1,24 +1,19 @@
-import { put, takeEvery, all } from 'redux-saga/effects';
+import { put, takeLatest, all } from 'redux-saga/effects';
 
-import { types, uniqueTypes } from 'types';
+import types from 'types';
 
-const delay = ms => new Promise(res => setTimeout(res, ms));
-
-function* helloSaga() {
-	console.log('Hello Sagas!');
+function* startLogin(action) {
+	const data = yield fetch('https://jsonplaceholder.typicode.com/todos/1')
+	yield console.log('data', {data});
+	yield console.log('action', action);
 }
 
-function* startLogin() {
-  yield delay(1000);
-	yield console.log('skoskos');
-}
-
-function* watchIncrementAsync() {
-	yield takeEvery(types[uniqueTypes.authStartLogin], startLogin);
+function* watchStartLogin() {
+	yield takeLatest(types.authStartLogin, startLogin);
 }
 
 // notice how we now only export the rootSaga
 // single entry point to start all Sagas at once
 export default function* authSaga() {
-	yield all([ helloSaga(), watchIncrementAsync() ]);
+	yield all([ watchStartLogin() ]);
 }
