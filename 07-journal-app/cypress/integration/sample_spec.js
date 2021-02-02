c2e897552978f517f6602703e9e7ceaf4b199fc4
Rodrigo <cuitlagua@gmail.com>
2021-02-01 22:00:21 -0600

    test/cypress visist test

--- 07-journal-app/cypress/integration/sample_spec.js
+++ 07-journal-app/cypress/integration/sample_spec.js
@@ -1,5 +1,5 @@
-describe('My First Cypress Test', () => {
-	it('Does not do much!', () => {
-		expect(true).to.equal(true);
-	});
-});
+describe('My First Test', () => {
+  it('Visits the Kitchen Sink', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
