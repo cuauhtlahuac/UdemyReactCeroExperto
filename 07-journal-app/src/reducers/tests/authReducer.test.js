3eb0e7ee7933442b42ea7b655ad883d4bf9af427
Rodrigo <cuitlagua@gmail.com>
2021-02-08 22:35:28 -0600

    test/auth reducer

--- 07-journal-app/src/reducers/tests/authReducer.test.js
+++ 07-journal-app/src/reducers/tests/authReducer.test.js
@@ -1,8 +1,32 @@
 import { authReducer } from 'reducers/authReducer';
+import { types } from 'types/types';
 
 describe('Auth Reducer Tests', () => {
-	test('should return an initial state', () => {
-    const state = authReducer({}, {type: 'noAction'});
-    expect(state).toEqual({})
+	const state = {
+		name: 'Ed',
+		password: 'sks565',
+		email: 'hola@pk2.com',
+	};
+
+	test('should return login state', () => {
+		const authState = authReducer(state, {
+			type: types.login,
+			payload: { uid: '6456454', displayName: 'Ender' },
+		});
+
+		expect(authState).toEqual({
+			uid: '6456454',
+			name: 'Ender',
+		});
+	});
+
+	test('should return logout state', () => {
+		const authState = authReducer(state, { type: types.logout });
+		expect(authState).toEqual({});
+	});
+
+	test('should return the state', () => {
+		const authState = authReducer(state, { type: 'noType' });
+		expect(authState).toEqual(state);
 	});
 });
